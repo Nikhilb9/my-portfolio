274eddaeec91399f37e581b1869670a41d41cd5e
Nikhil Baisoya <nikhil@larixtechnologies.com>
2021-04-23 21:26:00 +0530

    Updates

---
+++ precache-manifest.12f67d9399274c484b3a7b151ec9ea70.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58125e53e5675120ae8f8d9f6a77c46e",
+    "url": "/Nikhilb9/my-portfolio.git/index.html"
+  },
+  {
+    "revision": "ef7fad91519bdef3711d",
+    "url": "/Nikhilb9/my-portfolio.git/static/css/2.7b1f77b4.chunk.css"
+  },
+  {
+    "revision": "50216b26c36b538f298e",
+    "url": "/Nikhilb9/my-portfolio.git/static/css/main.e95d0b21.chunk.css"
+  },
+  {
+    "revision": "ef7fad91519bdef3711d",
+    "url": "/Nikhilb9/my-portfolio.git/static/js/2.a0046ea4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Nikhilb9/my-portfolio.git/static/js/2.a0046ea4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50216b26c36b538f298e",
+    "url": "/Nikhilb9/my-portfolio.git/static/js/main.18073844.chunk.js"
+  },
+  {
+    "revision": "bbc4bdff069937a175b7",
+    "url": "/Nikhilb9/my-portfolio.git/static/js/runtime-main.68fcbee1.js"
+  },
+  {
+    "revision": "3d45f423d62a5020c05c928ad8772391",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/arrow.3d45f423.svg"
+  },
+  {
+    "revision": "a74212b632dc205e2bd832acaa46a2e9",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/arrow.a74212b6.svg"
+  },
+  {
+    "revision": "04e4f259c38bea403acb2148d9f90aa0",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/contact-info-icon1.04e4f259.svg"
+  },
+  {
+    "revision": "df4fdedf2f261b6bd88551a4f1ab92f9",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/contact-info-icon2.df4fdedf.svg"
+  },
+  {
+    "revision": "3cf93bcfa4d4d754bc4675b37bc0361b",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/contact-info-icon3.3cf93bcf.svg"
+  },
+  {
+    "revision": "db57da795c9c521b14cf9f487da06d05",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/dribble.db57da79.svg"
+  },
+  {
+    "revision": "aa1e8c9c8592f04d2d3d68c86a746696",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/facebook.aa1e8c9c.svg"
+  },
+  {
+    "revision": "789bce482c497ca5006b97bfe6f0f91a",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/logo-yellow.789bce48.svg"
+  },
+  {
+    "revision": "f521cf4553d6d240c47e135be9ba7a9c",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/mobile-close.f521cf45.svg"
+  },
+  {
+    "revision": "10823520de33dffb05e6963a0d8c7bec",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/mobile-menu.10823520.svg"
+  },
+  {
+    "revision": "1f6de9f03ce7d4129313827d29a875d1",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner01.1f6de9f0.svg"
+  },
+  {
+    "revision": "d8f2a498f49a22e58484e1be708fbcf0",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner02.d8f2a498.svg"
+  },
+  {
+    "revision": "0d091731c341987d1a26f18053352817",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner03.0d091731.svg"
+  },
+  {
+    "revision": "81292e3cc0ec62e529d83a0df681f975",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner04.81292e3c.svg"
+  },
+  {
+    "revision": "d39e28a33d995a61c2338ac40eca3c67",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner05.d39e28a3.svg"
+  },
+  {
+    "revision": "a0c63ad5babbf86025666de007e4af70",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/partner06.a0c63ad5.svg"
+  },
+  {
+    "revision": "0ecdf21ae145f462c5258daad33c2dda",
+    "url": "/Nikhilb9/my-portfolio.git/static/media/twitter.0ecdf21a.svg"
+  }
+]);
